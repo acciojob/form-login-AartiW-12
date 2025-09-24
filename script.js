43060e48be3dd426a5d41e12c157f37776675596
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-24 11:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const firstName =  document.querySelector("input[name='fname']").value.trim();
+	const lastName  = document.querySelector("input[name='lname']").value.trim()
 
+	const fullName = `${firstName} ${lastName}`.trim();
+
+	alert(fullName)
 }
